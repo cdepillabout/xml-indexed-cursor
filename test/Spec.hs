b4b426d591ab13ec9cd395e9c4fe4470ace8314e
(cdep)illabout <cdep.illabout@gmail.com>
2017-02-22 15:25:18 +0900

    Get all tests working correctly.

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,14 +7,16 @@ module Main where
 import Data.ByteString.Lazy (ByteString)
 import Data.Default (def)
 import Data.Monoid ((<>))
-import Data.Text (Text)
+import Data.Text (Text, isPrefixOf)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit ((@?=), testCase)
 import Text.XML (parseLBS_, Element(..), Name(..), Node(..))
-import Text.XML.Cursor.Generic (($.//), child, descendant, node)
+import Text.XML.Cursor.Generic (($.//), ($//), child, descendant, node)
 
 import Text.XML.Cursor.Indexed
-       (IndexedCursor, IndexedNode(..), check, fromDocument)
+       (IndexedCursor, IndexedNode(..), attribute, attributeIs, check,
+        checkElement, checkIndexedNode, checkName, content, element,
+        fromDocument, hasAttribute, laxAttribute)
 
 main :: IO ()
 main = defaultMain tests
@@ -28,18 +30,13 @@ xmlCursorTests =
     "XML Cursors"
     [ cursorCheck
     , cursorPredicate
-    , cursorCheckNode
+    , cursorCheckIndexedNode
     , cursorCheckElement
     , cursorCheckName
-    , cursorAnyElement
     , cursorElement
-    , cursorLaxElement
     , cursorContent
     , cursorAttribute
     , cursorLaxAttribute
-    , cursorDeep
-    , cursorForce
-    , cursorForceM
     , cursorHasAttribute
     , cursorAttributeIs
     ]
@@ -53,47 +50,62 @@ cursorPredicate =
   testCase "has correct check with lists" $
   (name $ cursor $.// check descendant) @?= ["foo", "bar2", "baz3", "bar3"]
 
-cursorCheckNode :: TestTree
-cursorCheckNode = testCase "has correct checkNode" $ undefined
+cursorCheckIndexedNode :: TestTree
+cursorCheckIndexedNode =
+  testCase "has correct checkIndexedNode" $
+  (name $ cursor $// checkIndexedNode f) @?= ["bar1", "bar2", "bar3"]
+  where
+    f :: IndexedNode -> Bool
+    f IndexedNode{indexedNodeNode = NodeElement e} =
+      "bar" `isPrefixOf` nameLocalName (elementName e)
+    f _ = False
 
 cursorCheckElement :: TestTree
-cursorCheckElement = testCase "has correct checkElement" $ undefined
+cursorCheckElement =
+  testCase "has correct checkElement" $
+  (name $ cursor $// checkElement f) @?= ["bar1", "bar2", "bar3"]
+  where
+    f :: Element -> Bool
+    f e = "bar" `isPrefixOf` nameLocalName (elementName e)
+
 
 cursorCheckName :: TestTree
-cursorCheckName = testCase "has correct checkName" $ undefined
+cursorCheckName =
+  testCase "has correct checkName" $
+  (name $ cursor $// checkName f) @?= ["bar1", "bar2", "bar3"]
+  where
+    f :: Name -> Bool
+    f n = "bar" `isPrefixOf` nameLocalName n
 
-cursorAnyElement :: TestTree
-cursorAnyElement = testCase "has correct anyElement" $ undefined
 
 cursorElement :: TestTree
-cursorElement = testCase "has correct element" $ undefined
-
-cursorLaxElement :: TestTree
-cursorLaxElement = testCase "has correct laxElement" $ undefined
+cursorElement =
+  testCase "has correct element" $
+  (name $ cursor $// element "bar1") @?= ["bar1"]
 
 cursorContent :: TestTree
-cursorContent = testCase "has correct content" $ undefined
+cursorContent = testCase "has correct content" $ do
+  content cursor @?= []
+  (cursor $.// content) @?= ["a", "b"]
 
 cursorAttribute :: TestTree
-cursorAttribute = testCase "has correct attribute" $ undefined
+cursorAttribute =
+  testCase "has correct attribute" $ attribute "attr" cursor @?= ["x"]
 
 cursorLaxAttribute :: TestTree
-cursorLaxAttribute = testCase "has correct laxAttribute" $ undefined
-
-cursorDeep :: TestTree
-cursorDeep = testCase "has correct &* and $* operators" $ undefined
-
-cursorForce :: TestTree
-cursorForce = testCase "has correct force" $ undefined
-
-cursorForceM :: TestTree
-cursorForceM = testCase "has correct forceM" $ undefined
+cursorLaxAttribute =
+  testCase "has correct laxAttribute" $
+  (cursor $.// laxAttribute "Attr") @?= ["x", "y", "q"]
 
 cursorHasAttribute :: TestTree
-cursorHasAttribute = testCase "has correct hasAttribute" $ undefined
+cursorHasAttribute =
+  testCase "has correct hasAttribute" $
+  (length $ cursor $.// hasAttribute "attr") @?= 2
 
 cursorAttributeIs :: TestTree
-cursorAttributeIs = testCase "has correct attributeIs" $ undefined
+cursorAttributeIs =
+  testCase "has correct attributeIs" $
+  (length $ cursor $.// attributeIs "attr" "y") @?= 1
 
 name :: [IndexedCursor] -> [Text]
 name [] = []
@@ -125,46 +137,14 @@ cursor =
            "<Bar1 xmlns=\"http://example.com\" Attr=\"q\"/>" <>
         "</foo>"
 
-bar2, baz2, bar3, bin2 :: IndexedCursor
+bar2 :: IndexedCursor
 bar2 = child cursor !! 1
+
+baz2 :: IndexedCursor
 baz2 = child bar2 !! 1
 
+bar3 :: IndexedCursor
 bar3 = child cursor !! 2
+
+bin2 :: IndexedCursor
 bin2 = child bar3 !! 1
-
--- cursorPredicate = (name $ cursor $.// Cu.check Cu.descendant) @?= T.words "foo bar2 baz3 bar3"
--- cursorCheckNode = (name $ cursor $// Cu.checkNode f) @?= T.words "bar1 bar2 bar3"
---     where f (Res.NodeElement e) = "bar" `T.isPrefixOf` Res.nameLocalName (Res.elementName e)
---           f _               = False
--- cursorCheckElement = (name $ cursor $// Cu.checkElement f) @?= T.words "bar1 bar2 bar3"
---     where f e = "bar" `T.isPrefixOf` Res.nameLocalName (Res.elementName e)
--- cursorCheckName = (name $ cursor $// Cu.checkName f) @?= T.words "bar1 bar2 bar3"
---     where f n = "bar" `T.isPrefixOf` nameLocalName n
--- cursorAnyElement = (name $ cursor $// Cu.anyElement) @?= T.words "bar1 bar2 baz1 baz2 baz3 bar3 bin1 bin2 bin3 Bar1"
--- cursorElement = (name $ cursor $// Cu.element "bar1") @?= ["bar1"]
--- cursorLaxElement = (name $ cursor $// Cu.laxElement "bar1") @?= ["bar1", "Bar1"]
--- cursorContent = do
---   Cu.content cursor @?= []
---   (cursor $.// Cu.content) @?= ["a", "b"]
--- cursorAttribute = Cu.attribute "attr" cursor @?= ["x"]
--- cursorLaxAttribute = (cursor $.// Cu.laxAttribute "Attr") @?= ["x", "y", "q"]
-
--- cursorHasAttribute = (length $ cursor $.// Cu.hasAttribute "attr") @?= 2
--- cursorAttributeIs = (length $ cursor $.// Cu.attributeIs "attr" "y") @?= 1
-
--- cursorDeep = do
---   (Cu.element "foo" &/ Cu.element "bar2" &// Cu.attribute "attr") cursor @?= ["y"]
---   (return &.// Cu.attribute "attr") cursor @?= ["x", "y"]
---   (cursor $.// Cu.attribute "attr") @?= ["x", "y"]
---   (cursor $/ Cu.element "bar2" &// Cu.attribute "attr") @?= ["y"]
---   (cursor $/ Cu.element "bar2" &/ Cu.element "baz2" >=> Cu.attribute "attr") @?= ["y"]
---   null (cursor $| Cu.element "foo") @?= False
--- cursorForce = do
---   Cu.force DummyEx [] @?= (Nothing :: Maybe Integer)
---   Cu.force DummyEx [1] @?= Just (1 :: Int)
---   Cu.force DummyEx [1,2] @?= Just (1 :: Int)
--- cursorForceM = do
---   Cu.forceM DummyEx [] @?= (Nothing :: Maybe Integer)
---   Cu.forceM DummyEx [Just 1, Nothing] @?= Just (1 :: Int)
---   Cu.forceM DummyEx [Nothing, Just (1 :: Int)] @?= Nothing
-
